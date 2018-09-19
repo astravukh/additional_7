59917ea8b4932a25b7e5fa25d2635249d163263a
Maksim Astravukh <astravukh@gmail.com>
2018-09-19 19:51:19 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,248 @@
+
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    var array = [];
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            array.push(new Cell(matrix[i][j], i , j));            
+        }        
+    }    
+
+    array = findRecursive(array);    
+    
+    const initial = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ];
+
+    array.forEach(element => {
+        initial[element.horizontal][element.vertical] = element.value;
+    });
+
+    return initial;
+}
+
+class Cell{
+    constructor(value, horizontal, vertical){
+        this.value = value;
+        this.horizontal = horizontal;
+        this.vertical = vertical;
+        this.square = this.setSquare(horizontal, vertical);
+        this.can = [];
+    }
+    setSquare(horizontal, vertical){
+        if (horizontal<3) {
+            if(vertical < 3) return 1;
+            if(vertical < 6 && vertical > 2) return 2;
+            if(vertical < 9 && vertical > 5) return 3;            
+        }
+        if (horizontal<6 && horizontal > 2) {
+            if(vertical < 3) return 4;
+            if(vertical < 6 && vertical > 2) return 5;
+            if(vertical < 9 && vertical > 5) return 6;            
+        }
+        if (horizontal<9 && horizontal > 2) {
+            if(vertical < 3) return 7;
+            if(vertical < 6 && vertical > 2) return 8;
+            if(vertical < 9 && vertical > 5) return 9;            
+        }
+    }
+}
+
+function setCan(arr) {
+    var vertical = [];
+    var horizontal = [];
+    var square = [];
+    arr.forEach(cell => {
+        if (cell.value === 0) {
+            arr.forEach(element => {                    
+                if(element.value !== 0){
+                    if (element.vertical === cell.vertical) {
+                        vertical.push(element.value);
+                    }
+
+                    if (element.horizontal === cell.horizontal){
+                        horizontal.push(element.value);
+                    }
+
+                    if (element.square === cell.square){
+                        square.push(element.value);
+                    }                                                
+                }
+            });
+            cell.can =[];
+            for(var i = 1; i <= 9; i++){
+                if (vertical.indexOf(i) === -1 && horizontal.indexOf(i) === -1 && square.indexOf(i) === -1) {                        
+                    cell.can.push(i);
+                }
+            }
+            vertical = [];
+            horizontal = [];
+            square = [];
+        }            
+    });
+    return arr;        
+}
+
+function Solution (arr){
+    arr = setCan(arr);
+    var flag = true;
+    while(flag){
+        flag = false;
+        arr.forEach(cell => {
+            if(cell.value === 0 && cell.can.length === 1){
+                cell.value = cell.can[0];
+                cell.can = [];
+                arr = setCan(arr);
+                flag = true;    
+            }        
+        });
+
+        arr.forEach(cell => {
+            cell.can.forEach(canValue => {
+                var temp = true;
+                arr.forEach(element => {
+                    if((element.value === 0) && (element.vertical === cell.vertical) && !(element.horizontal === cell.horizontal && element.vertical === cell.vertical)){
+                        if (element.can.indexOf(canValue) !== -1) {
+                            temp = false;
+                        }
+                    }
+                });
+                if (temp && cell.value === 0) {
+                    cell.value = canValue;
+                    cell.can = [];
+                    arr = setCan(arr);
+                    flag = true;
+                }
+
+            });               
+        });
+        arr.forEach(cell => {
+            cell.can.forEach(canValue => {
+                var temp = true;
+                arr.forEach(element => {
+                    if((element.value === 0) && (element.horizontal === cell.horizontal) && !(element.horizontal === cell.horizontal && element.vertical === cell.vertical)){
+                        if (element.can.indexOf(canValue) !== -1) {
+                            temp = false;
+                        }
+                    }
+                });
+                if (temp && cell.value === 0) {
+                    cell.value = canValue;
+                    cell.can = [];
+                    arr = setCan(arr);
+                    flag = true;
+                }
+
+            });               
+        });
+        arr.forEach(cell => {
+            cell.can.forEach(canValue => {
+                var temp = true;
+                arr.forEach(element => {
+                    if((element.value === 0) && (element.square === cell.square) && !(element.horizontal === cell.horizontal && element.vertical === cell.vertical)){
+                        if (element.can.indexOf(canValue) !== -1) {
+                            temp = false;
+                        }
+                    }
+                });
+                if (temp && cell.value === 0) {
+                    cell.value = canValue;
+                    cell.can = [];
+                    arr = setCan(arr);
+                    flag = true;
+                }
+
+            });               
+        });
+
+        arr.forEach(cell => {
+            cell.can.forEach(canValue => {
+                var temp = true;
+                arr.forEach(element => {
+                    if((element.value === 0) && (element.square === cell.square) && !(element.horizontal === cell.horizontal && element.vertical === cell.vertical)){
+                        if (element.can.indexOf(canValue) !== -1) {
+                            temp = false;
+                        }
+                    }
+                });
+                if (temp && cell.value === 0) {
+                    cell.value = canValue;
+                    cell.can = [];
+                    arr = setCan(arr);
+                    flag = true;
+                }
+
+            });               
+        });
+    }
+    return arr;
+}
+
+function findRecursive(arr, choise = 0){
+    arr = Solution (arr);        
+    let complete = true;
+    arr.forEach(element => {
+        if (element.value === 0) {
+            complete = false;
+        }
+    });
+    if(complete){
+        return arr;
+    }
+    else{
+        let arrTemp = JSON.parse(JSON.stringify(arr));
+        for (let el of arrTemp){
+            if (el.can.length === 2) {
+                el.value = el.can[choise];
+                el.can = [];
+                break;
+            }
+        }
+        arrTemp = Solution (arrTemp);
+        let noError = true;
+        let noZero = true;
+        for (let el of arrTemp){
+            if (el.value === 0 && el.can.length === 0) {
+                noError = false;
+                break;
+            }
+
+            if (el.value === 0 && el.can.length !== 0) {
+                noZero = false;
+                break;
+            }
+
+        }
+
+        if(noError && noZero){
+            return arrTemp;
+        }
+        else if(noError){
+            if(findRecursive(arrTemp, 0) !== false){
+                return findRecursive(arrTemp, 0)
+            }
+            else{
+                if(findRecursive(arrTemp, 1) !== false){
+                    return findRecursive(arrTemp, 1);                        
+                }
+                else{
+                    return findRecursive(arr, 1);
+                }                    
+            }
+                           
+        }
+        else if(!noError){
+            if(choise === 0){
+                return findRecursive(arr, 1)
+            }
+            return false;
+        }
+    }
 }
